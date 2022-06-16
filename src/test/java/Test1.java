6726e451861bf2ad1bce12b58d4332a888ea38cb
Maria Zasypkina <maria.zasypkina@gmail.com>
2022-06-16 17:54:53 -0600

    Guru99 Selenium2 TestCase1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -15,8 +15,17 @@ public void CheckXPathTest(){
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://galeapps.gale.com/apps/udemy/auth?SAMLRequest=fZDdToNAEIVfhex9WQpC0wmQUNtEVEy0jRJvyBZXi7I%2FMruh%2BvQFjEm98WYyJ5lzZuaLkYlWQ2bNQT7wT8vROOuhNJKZRsmEHIzRCJS%2BsZYzrdEdG7dWgo6K2hcuvigb7MR55B1OHt%2F1iJOvE5J7pS5udFlk%2BH0hFqW6D1X%2F9L7tK76xx%2BvLV7v%2FiAIbVrtNXl0FMyYbsQyK%2FWrh3634kIFoeS7RMGmGWM%2F3Z140m0e7%2BQLCEIKlG3j%2BM3GOopUI0ysJsZ0ExbBBkExwBFPDNituYbgKdKeMqlVL0nichmlBd%2Bb%2F384QeTdyIekvl77v3QnCxARR0TEmpmfx6Y%2F6Szk9AQ%3D%3D&RelayState=I0XpMKpXMAsz4m7XoQ5owWjSw_eEuxJCfubk63u5_TEI_H3-anim93MbB72NBe");
+        driver.findElement(By.xpath("//input[contains(@value,'PUB')]")).click();
+        driver.findElement(By.xpath("//input[contains(@type, 'text')]")).sendKeys("Anythink Libraries");
+        driver.findElement(By.xpath("//button[contains(@class,'search-btn')]")).click();
 
-        driver.findElement(By.xpath("//button[contains(@type, 'button')]")).click();
+        WebElement result = driver.findElement(By.xpath("//div[@class='js-result-container result-container']"));
+        System.out.println(result.getText());
+//        WebElement locationList = driver.findElement(By.xpath("//li[@class='location-item']"));
+//        Assert.assertTrue(locationList.getText().toLowerCase().contains("anythink libraries"));
+
+        //Assert.assertEquals(driver.findElement(By.xpath("//span[contains(@class,'item-value location-item-value')]")).getText().toLowerCase(), "anythink libraries");
+
 
         driver.quit();
 }
